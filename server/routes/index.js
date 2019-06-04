74d24fb27c5e3b5a7f5b39a4b5e8f9e46b527629
Carlos Aranda <carlos.eao15@gmail.com>
2019-06-04 00:09:48 -0500

    .

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const url = require('url');
+
 
 router.get('/',(req,res)=>{
-    res.send('El beris se la come');
+    res.send(req.hostname);
 });
 
 module.exports = router;
