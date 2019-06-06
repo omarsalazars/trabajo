336f18f11be3996760f4ab99dddb9ae91cb253b5
omarsalazars <omarsalazarx@gmail.com>
2019-06-06 07:49:43 -0500

    views
    Login bueno :')

--- server/public/js/app.js
+++ server/public/js/app.js
@@ -1,4 +1,4 @@
-var app = angular.module('myApp',['ngRoute']);
+var app = angular.module('myApp',['ngRoute', 'ngStorage']);
 
 app.config(function($routeProvider){
     $routeProvider
@@ -14,19 +14,102 @@ app.config(function($routeProvider){
         templateUrl: '../enterprises.html',
         controller: 'enterprisesController'
     })
-    .when("/login",{
+        .when("/login",{
         templateUrl:'../login.html',
         controller: 'loginController'
     });
+});
+
+angular.module('myApp').run(function($rootScope, $http, $location, $localStorage, $templateCache){
+    // keep user logged in after page refresh
+    if ($localStorage.currentUser) {
+        $http.defaults.headers.common.Authorization = 'Bearer ' + $localStorage.currentUser.token;
+    }
+
+    // redirect to login page if not logged in and trying to access a restricted page
+    $rootScope.$on('$locationChangeStart', function (event, next, current) {
+        var publicPages = ['/login','/','/buscar','/signin'];
+        var restrictedPage = publicPages.indexOf($location.path()) === -1;
+        if (restrictedPage && !$localStorage.currentUser) {
+            $rootScope.hide=false;
+            $location.path('/login');
+        }
+    });
+});
+
+app.controller('navController',function($scope, $localStorage, $rootScope){
+    
+    checkLogin();
+    
+    $rootScope.$on('$routeChangeStart', function (event, next, current) {
+        checkLogin();
+    });
+    
+    function checkLogin(){
+        if($localStorage.currentUser){
+            $rootScope.hide = true;
+            $scope.email = $localStorage.currentUser.email;
+        }
+        else{
+            $rootScope.hide = false;
+        }
+    }
+    
 });
 
 app.controller('indexController',function($scope){
 
 });
 
-app.controller('loginController',function($scope){
-    
+app.controller('loginController',function($scope, $location, AuthenticationService){
+    $scope.login = function(){
+        $scope.loading = true;
+        AuthenticationService.Login($scope.user.email,$scope.user.password, function(result){
+            if (result === true) {
+                $location.path('/');
+            } else {
+                $scope.error = 'Username or password is incorrect';
+                $scope.loading = false;
+            } 
+        });
+    }
+
+    initController();
+
+    function initController() {
+        // reset login status
+        AuthenticationService.Logout();
+    }
 });
+
+/*
+app.controller('loginController',function($scope, $http, $httpParamSerializer, $localStorage){
+
+
+    $scope.login = function(){
+        $http({
+            method: 'POST',
+            url: 'http://localhost:3000/api/users/login',
+            headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+            transformRequest: $httpParamSerializer,
+            data:{
+                email: $scope.user.email,
+                password: $scope.user.password
+            }
+        }).then(function(response){
+            alert('animo se hizo la machaca');
+            console.log(response);
+            $localStorage.currentUser = {email:$scope.user.email, token: response.data.token};
+            // add jwt token to auth header for all requests made by the $http service
+            $http.defaults.headers.common.Authorization = 'Bearer ' + response.data.token;
+            callback(true);
+        }, function(response){
+            alert('Usuario o contraseña incorrectos :(');
+            callback(false);
+        });
+    };
+});
+*/
 
 app.controller('enterprisesController', function($scope){
     $scope.split_enterprises=[
@@ -69,25 +152,8 @@ app.controller('enterprisesController', function($scope){
     ];
 });
 
-app.controller('searchController',function($scope){
-    $scope.offers = [
-        {
-            puesto:'Desarrollador Web',
-            empresa: 'Glazzol',
-            ciudad : 'Aguascalientes',
-            salario: '$9000'
-        },
-        {
-            puesto:'Gerente',
-            empresa: 'Chuck E Cheese',
-            ciudad : 'Aguascalientes',
-            salario: '$4500'
-        },
-        {
-            puesto:'Desarrollador Web',
-            empresa: 'Glazzol',
-            ciudad : 'Aguascalientes',
-            salario: '$9000'
-        }
-    ];
+app.controller('searchController',function($scope,$http){
+    $http.get('api/offers').then(function(response){
+        $scope.offers = response.data.offers;
+    });
 });
---
+++ server/public/js/authentication.service.js
@@ -0,0 +1,40 @@
+angular
+    .module('myApp')
+    .factory('AuthenticationService', Service);
+
+function Service($http, $localStorage,$rootScope) {
+    var service = {};
+
+    service.Login = Login;
+    service.Logout = Logout;
+
+    return service;
+
+    function Login(email, password, callback) {
+        $http.post('/api/users/login', { email: email, password: password })
+            .then(function (response) {
+            // login successful if there's a token in the response
+            if (response.data.token) {
+                // store username and token in local storage to keep user logged in between page refreshes
+                $localStorage.currentUser = { email: email, token: response.data.token };
+
+                // add jwt token to auth header for all requests made by the $http service
+                $http.defaults.headers.common.Authorization = 'Bearer ' + response.data.token;
+                
+                
+                // execute callback with true to indicate successful login
+                callback(true);
+            } else {
+                // execute callback with false to indicate failed login
+                callback(false);
+            }
+        });
+    }
+
+    function Logout() {
+        // remove user from local storage and clear http auth header
+        $rootScope.hide = false;
+        delete $localStorage.currentUser;
+        $http.defaults.headers.common.Authorization = '';
+    }
+}
