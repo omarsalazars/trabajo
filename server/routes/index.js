6fc9df4dc4348f05c032fc5e2e5543837bc1706d
omarsalazars <omarsalazarx@gmail.com>
2019-06-06 09:38:41 -0500

    Revert "Revert "Merge branch 'master' of https://github.com/omarsalazars/trabajo""
    This reverts commit 553b3cf42c9a53add4d92264cf4a28a8069f8cb4.

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,10 +1,24 @@
 const express = require('express');
 const router = express.Router();
-const url = require('url');
+const jwt = require('jsonwebtoken');
 
+let User = require('../models/user');
 
-router.get('/',(req,res)=>{
-    res.send('El beris se la come en '+req.headers.host);
-});
+router.get('/verify', (req,res)=>{
+    let token = req.query.token;
+
+    jwt.verify(token, process.env.SEED,(err, decoded)=>{
+        if(err){
+            return res.status(401).json({
+                ok:false,
+                err
+            })
+        }
+        User.findOneAndUpdate({email:decoded.email}, {verified:true}, {new:true})
+        .exec((err,userDB)=>{
+            res.redirect('localhost:3000/login');
+        })
+    })
+})
 
 module.exports = router;
---
+++ server/routes/tests.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const router = express.Router();
+const nodemailer = require("nodemailer");
+
+router.get('/sendMail',async (req,res)=>{
+
+  // create reusable transporter object using the default SMTP transport
+  let transporter = nodemailer.createTransport({
+    host: "SMTP.Office365.com",
+    port: 587,
+    secure: false, // true for 465, false for other ports
+    auth: {
+      user: 'carlos.eao@hotmail.com', // generated ethereal user
+      pass: 'Electropollosaurio1' // generated ethereal password
+    }
+  });
+
+  // send mail with defined transport object
+  let info = await transporter.sendMail({
+    from: 'Carlos', // sender address
+    to: "carlos.eao15@gmail.com, omarsalazarx@gmail.com", // list of receivers
+    subject: "Hello ✔", // Subject line
+    text: "Hello world?", // plain text body
+    html: "<b>Hello world?</b>" // html body
+  });
+
+  console.log("Message sent: %s", info.messageId);
+  // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+
+  // Preview only available when sending through an Ethereal account
+  console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+  // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+
+  res.send('Enviado');
+})
+
+module.exports = router;
