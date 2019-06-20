f382ac670d872fd658c405d3ecc847e0dcad60ca
Carlos Aranda <carlos.eao15@gmail.com>
2019-06-20 08:58:57 -0500

    putamadre

--- server/helpers/mailing.js
+++ server/helpers/mailing.js
@@ -103,8 +103,40 @@ async function sendVerificationMail(email, liga){
 }
 
 
+async function sendUploadedAnswersMail(email, enterpriseName){
+
+    // create reusable transporter object using the default SMTP transport
+    let transporter = nodemailer.createTransport({
+        host: "SMTP.Office365.com",
+        port: 587,
+        secure: false, // true for 465, false for other ports
+        auth: {
+            user: process.env.EMAIL, // generated ethereal user
+            pass: process.env.EMAILPASSWD // generated ethereal password
+        }
+    });
+
+    // send mail with defined transport object
+    let info = await transporter.sendMail({
+    from: 'Pruebas', // sender address
+    to: email, // list of receivers
+    subject: `${enterpriseName}, Un usuario subió un archivo con las respuestas a una vacante`, // Subject line
+    text: "", // plain text body
+    html: "" // html body
+    });
+
+    console.log("Message sent: %s", info.messageId);
+    // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+
+    // Preview only available when sending through an Ethereal account
+    console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+    // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+}
+
+
 module.exports = {
     sendVerificationMail,
     sendNewInterestedMail,
-    sendApplicationUpdateMail
+    sendApplicationUpdateMail,
+    sendUploadedAnswersMail
 }
