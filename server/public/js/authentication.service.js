540cbc97d9df37f4e73444750907187e1efb4a2c
omarsalazars <omarsalazarx@gmail.com>
2019-06-20 08:27:36 -0500

    Ya te dice el error en login

--- server/public/js/authentication.service.js
+++ server/public/js/authentication.service.js
@@ -11,29 +11,39 @@ function Service($http, $localStorage,$rootScope) {
     return service;
 
     function Login(email, password, callback) {
-        $http.post('/api/users/login', { email: email, password: password })
-            .then(function (response) {
-            // login successful if there's a token in the response
-            console.log(response);
-            if (response.data.token) {
-                // store username and token in local storage to keep user logged in between page refreshes
-                $localStorage.currentUser = { 
-                    user : response.data.user,
-                    token: response.data.token
-                };
-                $rootScope.email = $localStorage.currentUser.user.email;
-                $rootScope.logged=true;
-                // add jwt token to auth header for all requests made by the $http service
-                $http.defaults.headers.common.Authorization = 'Bearer ' + response.data.token;
-                
-                
-                // execute callback with true to indicate successful login
-                callback(true);
-            } else {
+        $http.post(
+            '/api/users/login', 
+            { email: email, password: password }
+        ).then(
+            function success(response) {
+                // login successful if there's a token in the response
+                console.log(response);
+                if(response.data.err){
+                    alert(response.data.err.message);
+                    console.log(response.data);
+                }
+                if (response.data.token) {
+                    // store username and token in local storage to keep user logged in between page refreshes
+                    $localStorage.currentUser = { 
+                        user : response.data.user,
+                        token: response.data.token
+                    };
+                    $rootScope.email = $localStorage.currentUser.user.email;
+                    $rootScope.logged=true;
+                    // add jwt token to auth header for all requests made by the $http service
+                    $http.defaults.headers.common.Authorization = 'Bearer ' + response.data.token;
+
+
+                    // execute callback with true to indicate successful login
+                    callback(true);
+                }
+            },
+            function error(response){
                 // execute callback with false to indicate failed login
+                alert(response.data.err.message);
                 callback(false);
             }
-        });
+        );
     }
 
     function Logout() {
